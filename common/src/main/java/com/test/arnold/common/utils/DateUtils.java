8fe0aace34b2589c543d05677132033ca431d375
wzh <804713973@qq.com>
2021-12-10 11:38:04 +0800

    添加common公共jar包

---
+++ common/src/main/java/com/test/arnold/common/utils/DateUtils.java
@@ -0,0 +1,19 @@
+package com.test.arnold.common.utils;
+
+import cn.hutool.core.util.StrUtil;
+
+/**
+ * @Auther: arnold
+ * @Description:
+ * @Date: Created in 2021/12/9 19:40
+ * @Modified By:
+ */
+public class DateUtils {
+
+    public static void test(){
+        if (StrUtil.hasBlank("1")){
+
+        }
+    }
+
+}
---
+++ eureka-client/src/main/java/com/test/arnold/client/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.test.arnold.client.controller;
+
+/**
+ * @Auther: arnold
+ * @Description:
+ * @Date: Created in 2021/12/9 20:05
+ * @Modified By:
+ */
+public class TestController {
+    private static void test(){
+
+    }
+}
